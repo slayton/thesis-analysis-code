cb4e305812d519d4da55632cb420dd725b9d6be3
Stuart Layton <stuart.layton@gmail.com>
2013-01-23 11:28:22 -0500

    updated return values for script

--- bilateral_figures/generateFigure1_ProbRippleDetect.m
+++ bilateral_figures/generateFigure1_ProbRippleDetect.m
@@ -1,4 +1,4 @@
-function generateFigure1_ProbRippleDetect
+function [pir, pcr, pis, psc] = generateFigure1_ProbRippleDetect
 %%
 clear;
 fprintf('Loading Sleep\n');
@@ -63,45 +63,11 @@ for iEpoch = 1:size(eList,1)
     
 end
 %%
+pir = pIpsiRun; 
+pcr = pContRun;
+pis = pIpsiSlp;
+pcs = pContSlp;
 
 boxplot( [pIpsiRun', pContRun', pIpsiSleep', pContSlp'] );
-
-
-
-
-
-
-%%
-
-
-% e = dset_load_eeg('Bon', 4, 5, 1:30);
-d = dset_load_all('Bon', 4, 5);
-
-%%
-ts = dset_calc_timestamps(d.eeg(1).starttime, numel(d.eeg(1).data), d.eeg(1).fs);
-
-figure;
-
-ax = axes;
-ii = 0;
-for i = 1:numel(e) 
-    
-    if strcmp(e(i).area, 'CA1')
-        
-        if strcmp(e(i).hemisphere, 'left')
-            c = 'r';
-        else
-            c = 'g';
-        end
-        line_browser(ts, e(i).data + 500 * ii, 'Parent', ax, 'color', c);
-        ii = ii + 1;
-    end
-end
-%%
-set(ax, 'YLim', [-500 (ii+1)*500]);
-%%
-badTs = [5775 6320]
-badIdx = interp1(1:numel(ts), ts, [5775 6320], 'nearest')
-
 
 end
