95040c4aee9eca2af922d3e51512dff6f01d839a
Stuart Layton <stuart.layton@gmail.com>
2013-02-18 16:27:44 -0500

    checkin of process_data_set file for running all analysis

---
+++ DecodingPaperRevisions/process_data_set.m
@@ -0,0 +1,53 @@
+function process_data_set(baseDir)
+
+if ~exist(baseDir, 'dir')
+    error('%s does not exist!\n', baseDir);
+end
+
+
+klustDir = fullfile(baseDir, 'kKlust');
+
+% Create kKlust directory
+if ~exist(klustDir);
+    fprintf('Creating:%s\n', klustDir);
+    mkdir(klustDir);
+end
+
+
+% load .tt files and save s spikes.mat and waveforms.mat
+if ~exist( fullfile(klustDir, 'spikes.mat'), 'file') || ~exist( fullfile(klustDir, 'waveforms.mat'))
+    save_waveform_files(baseDir);
+else
+    fprintf('spike.mat and waveforms.mat already saved, skipping\n');
+end
+
+featFiles = dir( fullfile(klustDir, 'tt.fet.*') );
+if numel(featFiles) == 0
+    save_feature_files(baseDir);
+else
+    fprintf('Feature files already saved, skipping\n');
+end
+
+
+pcaFiles = dir( fullfile(klustDir, 'tt.pca.fet.*') );
+if numberl(pcaFiles) == 0
+    save_pca_feature_files(baseDir);
+else
+    fprintf('PCA feature files already saved, skipping\n');
+end
+
+
+clFiles = dir( fullfile(klustdir', 'tt.pca.clu.*' ));
+if numel(clFiles) == 0
+    cluster_feature_files(baseDir, 'tt.pca.clu');
+else
+    fprintf('PCA clusters already saved, skipping\n');
+end
+
+
+
+    
+
+
+
+end
